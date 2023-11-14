4dede147c5938764f056ac77c0349282a9e85d4f
HanaaBelle <hanaabelhamidi@gmail.com>
2023-11-14 19:00:36 +0100

    Added three user cards

--- random_users_cards_script.js
+++ random_users_cards_script.js
@@ -1,7 +1,7 @@
 // Affichage des "users_cards" de manière "Random"
 
 // Affecter à la constante "url_api_users_cards" l'URL de l'API "https://randomuser.me/api/"
-const url_api_users_cards = "https://randomuser.me/api/"
+const url_api_users_cards = "https://randomuser.me/api/?results=3"
 
 // Initialiser la requête "HTTP GET" avec la méthode "fetch" qui est appelé avec "url_api_users_cards", qui renvoie
 // ensuite une promesse qui se résout en un objet "response" qui représente la réponse à cette requête
@@ -38,16 +38,41 @@ fetch(url_api_users_cards)
        // Parcourir chaque utilisateur à partir de la liste des users avec le "forEach" et afficher ses informations
        users_list.forEach(user => {
            const user_name = `${user.name.title} ${user.name.first} ${user.name.last}`;
-           const user_gender = user.gender;
-           const user_nationality = user.nat;
-           console.log("Carte utilisateur:", { user_name , user_gender, user_nationality});
-       })
+           console.log("Carte utilisateur:", {user_name});
 
+           // Créer la carte de chaque user
+           const cardOfUser = `
+
+               <div class="container-center-horizontal">
+               
+                   <div class="container-card-box">
+                   
+                       <div class="card-box">
+                   
+                           <h1 id="${user_name}"> ${user_name} </h1>
+                           <h2> ${user.login.username} </h2>
+                           <img class="card-image" src="${user.picture.medium}" alt="${user_name} photo">
+                       
+                           <div class="card-body">
+                               <h2> ${user.gender} </h2>
+                               <h2> ${user.dob.age} Years Old</h2>
+                               <h3> ${user.email} </h3>
+                               <h2> ☏ ${user.cell} </h2>
+                               <h2> ${user.location.country} </h2>
+                           </div>
+                       </div>  
+                   </div> 
+               </div> 
+           `;
+           document.getElementById("user-card").innerHTML += cardOfUser;
+       })
     })
 
     // Attraper toutes les erreurs qui peuvent survenir pendant la requête ou le traitement de la réponse
     .catch(error => {
         console.error('Il y a eu un problème avec l\'opération de récupération "fetch" :', error);
     });
+
+
 
 
